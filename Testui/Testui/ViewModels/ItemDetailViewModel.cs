9c34254551c77edcce6100df9260e0e557d75144
Ayman Gassara <a.gassara@root.sa>
2018-04-17 09:17:08 +0100

    Add project files.

---
+++ Testui/Testui/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Testui.Models;
+
+namespace Testui.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
